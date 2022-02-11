677061f02c5d01edd26418fb2fbe96484e401c38
sunzc <sunzcdev@gmail.com>
2022-02-11 10:30:29 +0800

    splash screen

--- app/src/main/java/net/sunzc/myapplication2/MainActivity.java
+++ app/src/main/java/net/sunzc/myapplication2/MainActivity.java
@@ -3,19 +3,26 @@ package net.sunzc.myapplication2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.ArrayMap;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
 	private final ArrayMap<String, Class> demos = new ArrayMap<>();
+	private boolean show = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SplashScreen.installSplashScreen(this);
 		setContentView(R.layout.activity_main);
+		View contentView = findViewById(android.R.id.content);
+		contentView.getViewTreeObserver().addOnPreDrawListener(() -> show);
 		ListView demoListView = findViewById(R.id.demoList);
 		demos.put("ResultApi", MainActivity2.class);
 		demos.put("CameraX", CameraXActivity.class);
@@ -24,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
 			Class clazz = demos.get(demos.keyAt(position));
 			startActivity(new Intent(MainActivity.this, clazz));
 		});
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(3000);
+					show = true;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 
 }
